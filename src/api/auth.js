655ff2ac6bef1fe386b538ef9fbc8a589ca06f6a
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-13 11:02:02 -0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,12 @@
 import axios from "./axios";
 
-export const registerRequest = (user) => axios.post("api/register", user);
-export const loginRequest = (user) => axios.post("api/login", user);
+export const registerRequest = (user) => axios.post("register", user);
+export const loginRequest = (user) => axios.post("login", user);
 export const verifyTokenRequest = () =>
-  axios.get("api/verify", {
+  axios.get("verify", {
     withCredentials: true,
   });
 export const logoutRequest = () =>
-  axios.post("api/logout", {}, {
+  axios.post("logout", {}, {
     withCredentials: true,
   });
