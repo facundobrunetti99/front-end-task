8e254894c9ec76103bc5da53d8bbb070e6e7a952
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-13 13:43:01 -0300

    Update project.js

--- src/api/project.js
+++ src/api/project.js
@@ -1,8 +1,6 @@
 import axios from "./axios";
-export const getProjectsRequest = () => axios.get("/projects");
-export const getProjectRequest = (id) => axios.get(`/project/${id}`);
-export const createProjectRequest = (project) =>
-  axios.post("/project", project);
-export const updateProjectRequest = (id, project) =>
-  axios.put(`/project/${id}`, project);
-export const deleteProjectRequest = (id) => axios.delete(`/project/${id}`);
+export const getProjectsRequest = () => axios.get("/api/projects");
+export const getProjectRequest = (id) => axios.get(`/api/project/${id}`);
+export const createProjectRequest = (project) => axios.post("/api/project", project);
+export const updateProjectRequest = (id, project) => axios.put(`/api/project/${id}`, project);
+export const deleteProjectRequest = (id) => axios.delete(`/api/project/${id}`);
