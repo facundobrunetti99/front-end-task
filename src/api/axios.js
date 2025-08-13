b7d15d0a9f34a826b02579b4c756f4913b5c924b
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-13 13:27:33 -0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
   }
