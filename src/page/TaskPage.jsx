f3ba17f7899926057634c97713e6d8d7b0a54a02
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-13 15:59:37 -0300

    Update TaskPage.jsx

--- src/page/TaskPage.jsx
+++ src/page/TaskPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import { Link, useParams } from "react-router-dom";
 import { useTask } from "../components/context/TaskContext";
 import TaskCard from "../components/TaskCard";
