cb36bf3f43621bb012fc498549c9ac0cc03cb8e5
Facundo Garcia Brunetti <122062065+facundobrunetti99@users.noreply.github.com>
2025-08-13 13:47:44 -0300

    Update story.js

--- src/api/story.js
+++ src/api/story.js
@@ -1,16 +1,16 @@
 import axios from "./axios";
 
 export const getStoriesRequest = (projectId, epicId) =>
-  axios.get(`/projects/${projectId}/epics/${epicId}/stories`);
+  axios.get(`api/projects/${projectId}/epics/${epicId}/stories`);
 
 export const createStoryRequest = (projectId, epicId, story) =>
-  axios.post(`/projects/${projectId}/epics/${epicId}/story`, story);
+  axios.post(`api/projects/${projectId}/epics/${epicId}/story`, story);
 
 export const getStoryRequest = (projectId, epicId, storyId) =>
-  axios.get(`/projects/${projectId}/epics/${epicId}/story/${storyId}`);
+  axios.get(`api/projects/${projectId}/epics/${epicId}/story/${storyId}`);
 
 export const updateStoryRequest = (projectId, epicId, storyId, story) =>
-  axios.put(`/projects/${projectId}/epics/${epicId}/story/${storyId}`, story);
+  axios.put(`api/projects/${projectId}/epics/${epicId}/story/${storyId}`, story);
 
 export const deleteStoryRequest = (projectId, epicId, storyId) =>
-  axios.delete(`/projects/${projectId}/epics/${epicId}/story/${storyId}`);
+  axios.delete(`api/projects/${projectId}/epics/${epicId}/story/${storyId}`);
